f473075cd4ee97ae80d35a4fdf09b6bb4fbfd2d0
cl4pper <matheuspereiradesouza@MacBook-Air-de-Matheus.local>
2019-06-02 19:46:49 -0300

    [master] Test user commit

--- src/containers/LinksHeader/LinksHeader.jsx
+++ src/containers/LinksHeader/LinksHeader.jsx
@@ -9,7 +9,7 @@ import { RouteComponent } from '../../components/RouteLink/RouteLink'
 const LinksHeader = () => {
 	return (
 		<div className="LinksHeader">
-			<RouteComponent to="/" label="Title"></RouteComponent>
+			<RouteComponent to="/" label="Home"></RouteComponent>
 			<RouteComponent to="/list" label="List"></RouteComponent>
 		</div>
 	)
