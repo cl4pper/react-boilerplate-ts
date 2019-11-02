cffd91d955dcb42869d7b70c69ad3404338e3561
cl4pper <cl4pper@gmail.com>
2019-11-02 14:30:49 -0300

    [ADD] install and apply classNames package

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import classNames from 'classnames'
 
 // STYLE
 import './PageTitle.scss'
 
 function PageTitle (props) {
-  const { children } = props
+  const { children, light } = props
+
   return (
-    <h1 className="PageTitle">
+    <h1 className={classNames('PageTitle', {
+      'PageTitle--dark': !light,
+      'PageTitle--light': light
+    })}>
       {children}
     </h1>
   )
