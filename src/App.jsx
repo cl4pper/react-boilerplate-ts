6f974d9db4094597bf6e6cbad6b03b2da393235a
cl4pper <cl4pper@gmail.com>
2019-12-25 18:24:55 -0300

    ADD: set vertical orientation to ContentSection
    * Move social network links into top section

--- src/App.jsx
+++ src/App.jsx
@@ -4,59 +4,52 @@ import { connect } from 'react-redux'
 // STYLE
 import './app.scss'
 
-// STORE
-import { githubRequest } from '@store/modules/github'
-
 // COMPONENTS
 import { ReactComponent as ReactLogo } from '@assets/images/react-logo.svg'
 import { ReactComponent as GithubLogo } from '@assets/images/github-logo.svg'
 import { ReactComponent as Linkedin } from '@assets/images/linkedin-logo.svg'
+import { CommitsList } from '@containers'
 import { PageTitle, ContentSection, MainLink } from '@components'
 
 class AppContainer extends Component {
-  componentDidMount () {
-    this.props.loadData()
-  }
 
   render () {
-    const { strings, github } = this.props
-
-    // console.log(github.data)
+    const { strings } = this.props
 
     return (
       <div className="app">
-      <ContentSection dark height="50vh">
-        <PageTitle
-          subtitle="by cl4pper"
-          light>
-          {strings.title} <ReactLogo className="ReactLogo"/>
-        </PageTitle>
-      </ContentSection>
-      <ContentSection
-        light
-        spaced
-        height="50vh">
-        <MainLink
-          to="https://github.com/cl4pper/react-boilerplate"
-          logo={<GithubLogo />}>GitHub</MainLink>
-        <MainLink
-          to="https://www.linkedin.com/in/matheus-pereira-de-souza-19895316a/"
-          logo={<Linkedin />}>LinkedIn</MainLink>
-      </ContentSection>
-    </div>
+        <ContentSection dark height="50vh" vertical>
+          <PageTitle
+            subtitle="by cl4pper"
+            light>
+            {strings.title} <ReactLogo className="ReactLogo"/>
+          </PageTitle>
+          <div className="app__subsection">
+            <MainLink
+              to="https://github.com/cl4pper/react-boilerplate"
+              light
+              logo={<GithubLogo />}>GitHub</MainLink>
+            <MainLink
+              to="https://www.linkedin.com/in/matheus-pereira-de-souza-19895316a/"
+              light
+              logo={<Linkedin />}>LinkedIn</MainLink>
+          </div>
+        </ContentSection>
+        <ContentSection
+          light
+          spaced
+          height="50vh">
+          <CommitsList />
+        </ContentSection>
+      </div>
     )
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  loadData: () => dispatch(githubRequest())
-})
-
 const mapStateToProps = state => ({
   strings: state.strings.data,
-  github: state.github
 })
 
-const App = connect(mapStateToProps, mapDispatchToProps)(AppContainer)
+const App = connect(mapStateToProps)(AppContainer)
 
 export default App
