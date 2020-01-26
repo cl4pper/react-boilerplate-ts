47d2c5ffbe0be457b2e5b7d2fd3f1bacc1f28bc8
cl4pper <cl4pper@gmail.com>
2020-01-26 17:19:29 -0300

    style: lint filex by prettier

--- src/components/ContentSection/ContentSection.jsx
+++ src/components/ContentSection/ContentSection.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import classNames from 'classnames'
+import React from 'react';
+import classNames from 'classnames';
 
 // STYLE
-import './ContentSection.scss'
+import './ContentSection.scss';
 
-function ContentSection (props) {
-  const { children, light, left, right, spaced, height, vertical } = props
-  return (
-    <section className={classNames('ContentSection', {
-      'ContentSection--dark': !light,
-      'ContentSection--light': light,
-      'ContentSection--center': !left && !right && !spaced,
-      'ContentSection--right': right,
-      'ContentSection--left': left,
-      'ContentSection--spaced': spaced,
-      'ContentSection--vertical': vertical && !left && !right && !spaced,
-    })}
-    style={{height: `${height}`}}>
-      {children}
-    </section>
-  )
+function ContentSection(props) {
+	const { children, light, left, right, spaced, height, vertical } = props;
+	return (
+		<section
+			className={classNames('ContentSection', {
+				'ContentSection--dark': !light,
+				'ContentSection--light': light,
+				'ContentSection--center': !left && !right && !spaced,
+				'ContentSection--right': right,
+				'ContentSection--left': left,
+				'ContentSection--spaced': spaced,
+				'ContentSection--vertical': vertical && !left && !right && !spaced
+			})}
+			style={{
+				height: `${height}`
+			}}>
+			{children}
+		</section>
+	);
 }
 
-export {
-  ContentSection
-}
+export { ContentSection };
